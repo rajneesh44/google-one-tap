c7269075c3d1d52ee887ba0dd83217cf2d0db675
rajneesh44 <rajneeshsharma338@gmail.com>
2020-12-07 16:01:47 +0530

    components working fine

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,76 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from './auth.service';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { environment } from 'src/environments/environment';
+import firebase from 'firebase';
+
+declare var window: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  user: any;
+  isOneTap = true;
   constructor(
-  ) { }
+    private fireAuth: AngularFireAuth,
+    private changeDet: ChangeDetectorRef
+  ) {
+    this.fireAuth.user.subscribe(user => this.user);
+  }
+
+  async ngOnInit(): Promise<void> {
+    await this.init();
+  }
+
+  async init(): Promise<void> {
+    await this.onAuthStateChanged();
+  }
+
+  async onAuthStateChanged(): Promise<void> {
+    await this.fireAuth.onAuthStateChanged((user) => {
+      console.log(user);
+      this.user = user;
+      if (user) {
+        console.log(user?.displayName);
+        console.log(user?.email);
+        console.log(user?.uid);
+        console.log(user?.photoURL);
+        console.log(this.user);
+      }
+      if (!user) {
+        console.log('onetap block');
+        this.oneTap();
+      } else {
+        this.isOneTap = false;
+
+      }
+    });
+  }
+
+  oneTap(): void {
+    window.google.accounts.id.initialize({
+      client_id: environment.client_id,
+      cancel_on_tap_outside: false,
+      callback: async (token: any) => {
+        await this.handle(token);
+      }
+    });
+
+    window.google.accounts.id.prompt((notification) => {
+      console.log(notification.isNotDisplayed(), notification.isDisplayed());
+      if ((notification.isNotDisplayed() || notification.isSkippedMoment())) {
+        this.isOneTap = false;
+        this.changeDet.detectChanges();
+      }
+    });
+  }
+
+  async handle(token: any): Promise<void> {
+
+    const credential = firebase.auth.GoogleAuthProvider.credential(token.credential);
+    await this.fireAuth.signInWithCredential(credential);
+    this.isOneTap = true;
+  }
+
 }
--- src/app/one-tap/one-tap.component.ts
+++ src/app/one-tap/one-tap.component.ts
@@ -9,19 +9,9 @@ import { AuthService } from '../auth.service';
 })
 export class OneTapComponent implements OnInit {
   title = 'one-tap';
-  $user: any;
-  NotisOneTap = false;
+
   constructor(private authService: AuthService) { }
   async ngOnInit(): Promise<void> {
-    console.log(this.authService.user);
-    await this.authService.init();
-    if (this.authService.isOneTap) {
-      console.log('signIn with onetap');
-    } else {
-      console.log('singIn');
-    }
-    console.log('assacscas', this.NotisOneTap);
-    console.log('scscs', this.$user);
   }
 
   signOut(): void {
